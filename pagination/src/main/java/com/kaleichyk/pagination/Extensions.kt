d7c77e175a1e02f9d5f51ceb1edcbdcdad1884cf
Ivan <64867022+KoleychikIvan@users.noreply.github.com>
2021-05-29 16:34:05 +0300

    updated the tag and removed unnecessary elements in Extensions.kt

--- pagination/src/main/java/com/kaleichyk/pagination/Extensions.kt
+++ pagination/src/main/java/com/kaleichyk/pagination/Extensions.kt
@@ -1,19 +1,14 @@
 package com.kaleichyk.pagination
 
-import android.content.Context
 import android.util.Log
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 
-private const val TAG = "MAIN_APP_TAG"
-
-fun Context.showToast(text: String) {
-    Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
-}
+private const val TAG = "PAGINATION_TAG"
 
 fun showLog(text: String) {
     Log.d(TAG, text)
 }
 
-fun ViewGroup.getView(res: Int) = LayoutInflater.from(context).inflate(res, this, false)
+fun ViewGroup.getView(res: Int): View = LayoutInflater.from(context).inflate(res, this, false)
